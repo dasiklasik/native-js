dd79d205f8d2e68ab871e87f4489c46557b04bb2
dasiklasik <daryasamsonowicz@gmail.com>
2022-08-05 13:57:43 +0300

    learn __proto__ and prototype

---
+++ lesson 13/index.js
@@ -0,0 +1,45 @@
+
+
+//prototype
+
+// const use = () => {}
+//
+// console.dir(use)
+
+// const u1 = new User()
+//
+// console.log(u1)
+//
+// const u2 = new u1.constructor()
+//
+// console.log(u2)
+
+
+
+
+//__proto__
+// const baseUser = {
+//     login: 'default',
+// }
+//
+// const masha = {
+//     name: 'Masha'
+// }
+//
+// console.log(masha.login) //undefined
+//
+// masha.__proto__ = baseUser
+//
+// console.log(masha.login)
+
+// const obj = {}
+
+
+// function foo() {}
+// console.dir(foo)
+
+// function User() {}
+//
+// const user1 = new User()
+//
+// console.dir(user1.__proto__.__proto__)
