ff8e59d51e0a427812640447cd26c21341e2f010
dasiklasik <daryasamsonowicz@gmail.com>
2022-08-06 11:53:15 +0300

    learn class

---
+++ lesson-14/index.js
@@ -0,0 +1,54 @@
+//конструкторы
+
+// function UserС(name) {
+//     this.name = name
+// }
+//
+// User.prototype.hello = function () {
+//     console.log('Hello')
+// }
+//
+// const user1 = new User('Dasha')
+//
+// user1.hello()
+
+
+//классы
+// class User {
+//     constructor(name) {
+//         this.name = name
+//     }
+//
+//     showName() {
+//         console.log(this.name)
+//     }
+// }
+//
+// const user1 = new User('Dasha')
+//
+// user1.showName()
+
+// class User {
+//     static DEFAULT_AGE = 18
+// }
+//
+// console.dir(User)
+
+// class Car {
+//     constructor(name) {
+//         this.name = name
+//     }
+//
+//     run() {
+//         console.log('run')
+//     }
+//
+//     static some = '11'
+// }
+//
+// class Ford extends Car {
+//
+// }
+//
+// console.dir(Ford)
+
